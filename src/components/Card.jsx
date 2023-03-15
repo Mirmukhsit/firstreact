a4a7b5c3c9597d91dfc4547568bd1ebe93e35343
Mirmukhsit <botirovmirmuxsit@gmail.com>
2023-03-15 11:44:35 +0500

    child

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import './style.css'
-function Card(props) {
+import ChildCom from './Childcom';
+
+function Cards({datas}) {
+    console.log(datas);
   return (
-    <div className='wrapper'>
-        <img style={{width:"400px",height:"300px"}} src={props.src} alt="banan" />
-        <h1>{props.title}</h1>
-        <h4>{props.info}</h4>
-        <h4>{props.info}</h4>
-        <h4>{props.info}</h4>
-        <h4>{props.info}</h4>
+    <div>
+        <h1>Cards Com</h1>
+        <h3>{datas[0].id} {datas[0].names}</h3>
+        <h3>{datas[1].id} {datas[1].names}</h3>
+        <h3>{datas[2].id} {datas[2].names}</h3>
+        <h3>{datas[3].id} {datas[3].names}</h3>
+        <h3>{datas[4].id} {datas[4].names}</h3>
+        <hr />
+        <ChildCom data={datas}/>
     </div>
   )
 }
 
-export default Card
+export default Cards
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,29 +1,17 @@
-import React from "react";
-import Card from "./Card";
-import './style.css'
+import React from 'react'
 
-const data=[
-    {id:1,title : 'banan', info:'mevalar berilgan1',src:'https://cdn.shopify.com/s/files/1/0018/8327/5325/products/6_370x.jpg?v=1669704783%20370w' },
-    {id:2,title:'colorful', info:'mevalar berilgan2',src:'https://media.cntraveler.com/photos/546976f2cbbd2620680d9f2f/master/pass/rare-fruits-tout.jpg'},
-    {id:3,title:'nok', info:'mevalar berilgan3',src:'https://hips.hearstapps.com/hmg-prod/images/pile-of-ripe-fresh-green-pears-royalty-free-image-133810495-1564414665.jpg' },
-    {id:4,title:'qizil nok', info:'mevalar berilgan4',src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBQFxawL8hGHUNd2yW5j2_UZfGwoCMUVGmkCxUER8de_2mHdNc1lDC-5Q0uIyWmJElktY&usqp=CAU'},
-    {id:5, title:'olma', info:'mevalar berilgan5',src:'https://static.libertyprim.com/files/familles/pomme-large.jpg?1569271834' },
-    {id:6,title:'kivi', info:'mevalar berilgan6',src:'https://hips.hearstapps.com/hmg-prod/images/full-frame-shot-of-kiwi-slices-royalty-free-image-742260967-1564092068.jpg'},
-    {id:7, title:'asarti', info:'mevalar berilgan7',src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIJtfcEMDG_KCr4IcDO8IZlBBLuvhtozBwtw&usqp=CAU'},
-    {id:8, title:'ananas va banan', info:'mevalar berilgan8',src:'https://www.dole.com/-/media/project/dole/produce-images/headers/dole-produce-fruit-medley.png?rev=1416123f2d094cd1b7494365948214be&hash=F89C9786C9A5F599A784D7753F82236C'},
-  ]
+function Container({datas}) {
+  return (
+    <div>
+        <h1>Container</h1>
+        <h3>{datas[0].id} {datas[0].names} {datas[0].email} {datas[0].address}</h3>
+        <h3>{datas[1].id} {datas[1].names} {datas[1].email} {datas[1].address}</h3>
+        <h3>{datas[2].id} {datas[2].names} {datas[2].email} {datas[2].address}</h3>
+        <h3>{datas[3].id} {datas[3].names} {datas[3].email} {datas[3].address}</h3>
+        <h3>{datas[4].id} {datas[4].names} {datas[4].email} {datas[4].address}</h3>
+        <hr />
+    </div>
+  )
+}
 
-function Container() {
-    return (
-      <div className='container'>
-        {
-          data.map((val)=>(
-            <Card src={val.src} title={val.title} info={val.info}/>
-          ))
-        }
-          
-      </div>
-    )
-  }
-  
-  export default Container
+export default Container
