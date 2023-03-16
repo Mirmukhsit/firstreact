f580745a0efea0737b19f2002956bc4e73ea31dc
Mirmukhsit <botirovmirmuxsit@gmail.com>
2023-03-16 11:37:46 +0500

    search / delete / add by.id.name.address

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,53 +2,91 @@ import React, {useState} from 'react'
 import { data } from '../Moc';
 
 function Map() {
-  const [search, setSearch]=useState("")
-  const [datas, setData]=useState(data)
-  const onSearch = (event) => {
-    setSearch(event.target.value);
-    let res = datas.filter(val=>val.name.includes(search))
-    setData(res)
+    const [search, setSearch] = useState("");
+    const [select, setSelect] = useState("id");
+    const [datas, setData] = useState(data);
+    const [name, setName] = useState("");
+    const [address, setAddress] = useState("");
+    const onSearch = (event) => {
+      setSearch(event.target.value);
+      let res = datas.filter(val=>`${val[select]}`.toLowerCase().includes(search))
+      setData(res)
+    }
+    const getSelectedVal = (e) =>{
+      setSelect(e.target.value)
+    }
+    console.log(select);
+    const onDelete = (id) =>{
+      let result = datas.filter(val=>val.id !== id)
+      setData(result)
+    } 
+    const getValueName = (e) =>{
+      setName(e.target.value)
+    }
+    const getValueAddress = (e) =>{
+      setAddress(e.target.value)
+    }
+  
+    const onCreate = () =>{
+      let user = {
+        id:Date.now(),
+        name:name,
+        address:address,
+      }
+      let res = [...datas,user];
+      name && address && setData(res)
+      setName("");
+      setAddress("")
+    }
+    return (
+      <div>
+        <input value={name} onChange={getValueName} type="text" placeholder='name' />
+        <input value={address} onChange={getValueAddress} type="text" placeholder='address' />
+        <button onClick={onCreate}>add</button>
+        <br />
+        <br />
+        <hr />
+        <input type="text" placeholder='search' onChange = {onSearch} />
+        <select onChange={getSelectedVal} name="" id="">
+          <option value="id">id</option>
+          <option value="name">name</option>
+          <option value="address">address</option>
+        </select>
+        <table border="1" width={"70%"}>
+          <thead>
+            <tr>
+              <th>id </th>
+              <th>name</th>
+              <th>address</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              datas.length ?
+              datas.map((val,inx)=>{
+                return(
+                  <tr key = {val.id}>
+                    <td>{val.id}</td>
+                    <td>{val.name}</td>
+                    <td>{val.address}</td>
+                    <td>
+                      <button onClick={() => onDelete(val.id)}>delete</button>
+                      <button >edit</button>
+                    </td>
+                  </tr>
+                )
+              }) : 
+              <tr>
+                  <th colSpan={"4"}><h1>No Data</h1></th>
+              </tr>
+            }
+          </tbody>
+        </table>
+      </div>
+    )
   }
-  const onDelete = (id) =>{
-    let result = datas.filter(val=>val.id !== id)
-    setData(result)
-  } 
-  return (
-    <div>
-      <input type="text" placeholder='search' onChange = {onSearch} />
-      <table border="1">
-        <thead>
-          <tr>
-            <th>id </th>
-            <th>age</th>
-            <th>name</th>
-            <th>address</th>
-            <th>email</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            datas.map((val,inx)=>{
-              return(
-                <tr key = {val.id}>
-                  <td>{val.id}</td>
-                  <td>{val.age}</td>
-                  <td>{val.name}</td>
-                  <td>{val.address}</td>
-                  <td>{val.email}</td>
-                  <td>
-                    <button onClick={() => onDelete(val.id)}>delete</button>
-                  </td>
-                </tr>
-              )
-            })
-          }
-        </tbody>
-      </table>
-    </div>
-  )
-}
-
-
-export default Map;
+  
+  
+  export default Map;
+  
