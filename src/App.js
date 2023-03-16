ab242570839cd2668cbb5a57c55ff48842e83da1
Mirmukhsit <botirovmirmuxsit@gmail.com>
2023-03-16 11:29:05 +0500

    search and delete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react'
-import Cards from './components/Card'
-// import ChildCom from './components/ChildCom'
-import Container from './components/Container'
- 
+import Map from './components/Map';
+
+
 const data = [
-  {id:1,names:"Bobur",age:23, email:"bbbbbbbbbb@gmail.com",address:"Tashkent"},
+  {id:1,names:"Bobur",age:23,  email:"bbbbbbbbbb@gmail.com",address:"Tashkent"},
   {id:2,names:"Sardor",age:17, email:"sssssss@gmail.com",address:"Samarqand"},
-  {id:3,names:"Azim",age:25, email:"aaaaaaa@gmail.com",address:"Jizzax"},
-  {id:4,names:"Temur",age:12, email:"tttttttt@gmail.com",address:"Buxoro"},
+  {id:3,names:"Azim",age:25,   email:"aaaaaaa@gmail.com",address:"Jizzax"},
+  {id:4,names:"Temur",age:12,  email:"tttttttt@gmail.com",address:"Buxoro"},
   {id:5,names:"Davron",age:24, email:"dddddd@gmail.com",address:"Xorazm"},
-]
+];
+
 function App() {
   return (
     <div style={{padding:"20px"}}>
       <h1>App</h1>
-      <Cards datas={data}/>
-      sdasd
-      <Container datas={data}/>
+      <Map />
     </div>
   )
 }
---
+++ src/components/Map.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+import { data } from '../Moc';
+
+function Map() {
+  const [search, setSearch]=useState("")
+  const [datas, setData]=useState(data)
+  const onSearch = (event) => {
+    setSearch(event.target.value);
+    let res = datas.filter(val=>val.name.includes(search))
+    setData(res)
+  }
+  const onDelete = (id) =>{
+    let result = datas.filter(val=>val.id !== id)
+    setData(result)
+  } 
+  return (
+    <div>
+      <input type="text" placeholder='search' onChange = {onSearch} />
+      <table border="1">
+        <thead>
+          <tr>
+            <th>id </th>
+            <th>age</th>
+            <th>name</th>
+            <th>address</th>
+            <th>email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            datas.map((val,inx)=>{
+              return(
+                <tr key = {val.id}>
+                  <td>{val.id}</td>
+                  <td>{val.age}</td>
+                  <td>{val.name}</td>
+                  <td>{val.address}</td>
+                  <td>{val.email}</td>
+                  <td>
+                    <button onClick={() => onDelete(val.id)}>delete</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+
+export default Map;
